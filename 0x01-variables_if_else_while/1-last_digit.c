dfe8a6cc09fb9438548b45e240084a02f5195ab5
DennisKia <denniskanyi9@gmail.com>
2022-03-13 22:35:47 -0700

    Assigning a random number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,16 +7,17 @@
  * everytime it executes
  * Return; 0 if successful
  */
+int main(void)
 {
 	int n;
 
 	srand(time(0));
-	n=rand() - RAND_MAX / 2;
-	if (n>0);
+	n = rand() - RAND_MAX / 2;
+	if (n > 0);
 	printf("%d is positive\n", n);
-	else if (n==0)
+	else if (n == 0)
 		printf("%d is zero\n", n);
-	esle if (n<0)
+	esle if (n < 0)
 		printf("%d is negative\n", n);
 	return (0);
 
