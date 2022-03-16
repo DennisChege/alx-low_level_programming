04dca3ea0ac24e51ed43d0e6097d6afaffcca807
DennisKia <denniskanyi9@gmail.com>
2022-03-16 10:57:48 -0700

    program that prints _putchar, followed by a new line

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,1 @@
-#include <unistd.h>
-
-/**
- * main - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately
- */
-int _putchar.c_:(char c)
-{
-	return (write(1, &c, 1));
-}
+int _putchar(char c);
