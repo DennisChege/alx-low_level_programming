3d9e6ea061b67b707e9e0a37196df7f4d234838f
DennisKia <denniskanyi9@gmail.com>
2022-03-16 11:15:14 -0700

    program that prints _putchar, followed by a new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * putchar - writes the character c to stdout
+ * main - writes the character c to stdout
  * @c: The character to print
  *
  * Return: On success 0
  */
 int _putchar(char c)
 {
-	return (putchar(c));
+	return (_putchar(c));
 }
